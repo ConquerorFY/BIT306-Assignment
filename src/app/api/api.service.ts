d32df79d022f1bd5714e7ce4483c08329eabc293
ConquerorFY <ryanrlfy33@gmail.com>
2023-03-25 11:50:20 +0800

    Added Frontend API Services

---
+++ src/app/api/api.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:3000/api/';
+  private departmentUrl = "department";
+  private employeeUrl = "employee";
+  private fwaUrl = "fwa";
+  private scheduleUrl = "schedule";
+
+  constructor(private http: HttpClient) { }
+
+  getAllDepartments() {
+    return this.http.get(`${this.baseUrl + this.departmentUrl}/departments`);
+  }
+
+  getEmployeeFWARequestsByDepartment(data: any) {
+    return this.http.post(`${this.baseUrl + this.departmentUrl}/getEmployeeFWARequestsByDepartment`, data);
+  }
+
+  getEmployeeScheduleByDepartment(data: any) {
+    return this.http.post(`${this.baseUrl + this.departmentUrl}/getEmployeeScheduleByDepartment`, data);
+  }
+
+  registerAccount(data: any) {
+    return this.http.post(`${this.baseUrl + this.employeeUrl}/registerAccount`, data);
+  }
+
+  login(data: any) {
+    return this.http.post(`${this.baseUrl + this.employeeUrl}/login`, data);
+  }
+
+  logout(data: any) {
+    return this.http.post(`${this.baseUrl + this.employeeUrl}/logout`, data);
+  }
+
+  getEmployeeFWACount() {
+    return this.http.get(`${this.baseUrl + this.fwaUrl}/getEmployeeFWACount`);
+  }
+
+  insertFWA(data: any) {
+    return this.http.post(`${this.baseUrl + this.fwaUrl}/insertFWA`, data);
+  }
+
+  getFWA(data: any) {
+    return this.http.post(`${this.baseUrl + this.fwaUrl}/getFWA`, data);
+  }
+
+  updateFWA(data: any) {
+    return this.http.post(`${this.baseUrl + this.fwaUrl}/updateFWA`, data);
+  }
+
+  insertSchedule(data: any) {
+    return this.http.post(`${this.baseUrl + this.scheduleUrl}/insertSchedule`, data);
+  }
+
+  getSchedule(data: any) {
+    return this.http.post(`${this.baseUrl + this.scheduleUrl}/getSchedule`, data);
+  }
+
+  getSchedules(data: any) {
+    return this.http.post(`${this.baseUrl + this.scheduleUrl}/getSchedules`, data);
+  }
+
+  updateSchedule(data: any) {
+    return this.http.post(`${this.baseUrl + this.scheduleUrl}/updateSchedule`, data);
+  }
+}
